c483d0f0c780ee75b590f5720b8d4b9d13805071
StephhyL <Stephanie Li>
2021-10-25 15:34:46 -0400

    added tail.js which returns a new array with all expect index 0 of inputed array

---
+++ tail.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ?
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`)
+    : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}.`);
+};
+
+/* I misunderstood the assignment and modified the assertEqual function instead.
+const assertEqual = function(actual, expected){
+  for (let i = 0; i < actual.length; i++){
+    if (actual[i] !== expected[i]){
+      console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}.`);
+    }
+  }
+  if (actual.length !== expected.length){
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}.`);
+  }
+  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`);
+};
+*/
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]);
+
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+console.log(tail([1]));
+console.log(tail([]));
