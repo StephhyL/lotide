b4f5323c64a578e9a7e9dfdba6ad0926f3df7598
StephhyL <Stephanie Li>
2021-10-26 17:14:38 -0400

    modified head and tail js

--- tail.js
+++ tail.js
@@ -4,19 +4,6 @@ const assertEqual = function(actual, expected) {
     : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}.`);
 };
 
-/* I misunderstood the assignment and modified the assertEqual function instead.
-const assertEqual = function(actual, expected){
-  for (let i = 0; i < actual.length; i++){
-    if (actual[i] !== expected[i]){
-      console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}.`);
-    }
-  }
-  if (actual.length !== expected.length){
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}.`);
-  }
-  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`);
-};
-*/
 
 const tail = function(arr) {
   return arr.slice(1);
@@ -35,3 +22,18 @@ assertEqual(result[1], "Labs");
 
 console.log(tail([1]));
 console.log(tail([]));
+
+
+/* I misunderstood the assignment and modified the assertEqual function instead.
+const assertEqual = function(actual, expected){
+  for (let i = 0; i < actual.length; i++){
+    if (actual[i] !== expected[i]){
+      console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}.`);
+    }
+  }
+  if (actual.length !== expected.length){
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}.`);
+  }
+  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`);
+};
+*/
