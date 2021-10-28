394ab6b0e1df34543e6e100fc4fa87eb1cd312b9
StephhyL <Stephanie Li>
2021-10-28 15:26:02 -0400

    added return to assertArraysEqual to fix bug

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,12 @@
 const assertArraysEqual = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
-    console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
+    return console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
   }
   
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
+      return console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
     }
   }
-  console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}.`);
+  return console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}.`);
 };
