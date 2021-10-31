da0c79ee87f8d5032a296eccaee8b77e71f84df5
StephhyL <Stephanie Li>
2021-10-31 13:19:08 -0400

    updated assertArraysEqual in middle.js

--- middle.js
+++ middle.js
@@ -12,16 +12,8 @@ const eqArrays = (arr1, arr2) => {
 };
 
 const assertArraysEqual = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
-  }
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
-    }
-  }
-  console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}.`);
+  eqArrays(arr1, arr2)? console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}.`)
+  : console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
 };
 
 const middle = (arr) => {
