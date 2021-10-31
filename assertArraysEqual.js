62d9b8901a51cfc978ed3a44b06e3d691ef8c578
StephhyL <Stephanie Li>
2021-10-31 13:13:30 -0400

    fixed assertArraysEqual by leveraging eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,19 @@
-const assertArraysEqual = (arr1, arr2) => {
+const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
-    return console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
+    return false;
   }
   
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
+      return false;
     }
   }
-  return console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}.`);
+  return true;
 };
+
+const assertArraysEqual = (arr1, arr2) => {
+  eqArrays(arr1, arr2)? console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}.`)
+  : console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
+};
+
+//assertArraysEqual([1,2,4], [1,2,3]);
