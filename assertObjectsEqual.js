d1a8d9016162abae3ae6f77d3c7e5cafaeb44cda
StephhyL <Stephanie Li>
2021-10-31 13:30:00 -0400

    updated eassertObjectsEqual by leveraging eqObjects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -11,25 +11,28 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  if (Object.keys(actual).length !== Object.keys(expected).length) {
-    console.log(`🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}.`);
-    return;
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
   }
-  for (const key in actual) { 
-    if (Array.isArray(actual[key])) {
-      if(!(eqArrays(actual[key], expected[key]))){
-        console.log(`🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}.`);
-        return;
-      }
-    } else if (actual[key] !== expected[key]) { 
-       console.log(`🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}.`);
-       return;
+
+  for (const key in object1) {
+    
+    if (Array.isArray(object1[key])) {
+      return eqArrays(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) {
+      return false;
     }
   }
-  console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}.`);
-  return;
+  return true;
+};
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
+  eqObjects(actual, expected) ? console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}.`)
+    : console.log(`🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}.`);
 };
 
 const ab = { a: "1", b: "2"};
