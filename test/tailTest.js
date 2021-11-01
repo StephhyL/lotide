77752a8d87b1a9ab124e8f96aaa2b02267707496
StephhyL <Stephanie Li>
2021-11-01 13:54:13 -0400

    used M&C on middleTest.js  to test middle.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -15,13 +15,3 @@ describe ("#tail", () => {
 
 
 })
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words);
-// assertEqual(words.length, 3);
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result, ["Lighthouse", "Labs"]);
-// assertEqual(result.length, 2);
-// assertEqual(result[0], "Lighthouse");
-// assertEqual(result[1], "Labs");
