364a0a2dba4c754f67a0d786685a465ec72affaf
StephhyL <Stephanie Li>
2021-11-01 13:03:18 -0400

    added testfile for assertArraysEqual, exported and imported to refract

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,8 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   eqArrays(arr1, arr2) ? console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}.`)
     : console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
 };
 
-//assertArraysEqual([1,2,4], [1,2,3]);
+module.exports = assertArraysEqual;
