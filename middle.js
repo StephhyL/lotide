bd062f77e6edc0f9473c58710df3b5201b658d5b
StephhyL <Stephanie Li>
2021-11-01 13:09:40 -0400

    exported middle, in test file: imported assertArraysEqual and middle

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  eqArrays(arr1, arr2) ? console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}.`)
-    : console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}.`);
-};
-
 const middle = (arr) => {
   let result = [];
 
@@ -33,9 +15,4 @@ const middle = (arr) => {
   return result;
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+module.exports = middle;
