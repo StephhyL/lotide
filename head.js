60079ca40529ea87f2518f5aec4b5c0da8000fd5
StephhyL <Stephanie Li>
2021-10-31 23:27:46 -0400

    code for assertEqual and head using module.exports and require

--- head.js
+++ head.js
@@ -1,9 +1,4 @@
-const assertEqual = require('./assertEqual');
 const head = function(arr) {
   return arr[0];
 };
-
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([1]), 3);
-// assertEqual(head([]), 3);
+module.exports = head;
