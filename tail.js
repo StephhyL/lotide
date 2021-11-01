0ab795f3f6e55992c89f7224f4c4fe3dd04460c7
StephhyL <Stephanie Li>
2021-10-31 23:33:31 -0400

    refracted tail using node assert/modules.exports/require

--- tail.js
+++ tail.js
@@ -1,27 +1,8 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ?
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`)
-    : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}.`);
-};
-
-
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-console.log(tail([1]));
-console.log(tail([]));
+module.exports = tail;
 
 
 /* I misunderstood the assignment and modified the assertEqual function instead.
